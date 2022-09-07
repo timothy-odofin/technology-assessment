9e808afa301bc79d8efbdce9e2d214fc834ad068
Odofin Oyejide <odofin@swipe.ng>
2022-09-07 22:32:59 +0100

    added transactional aware annotation

--- src/main/java/technology/assessment/app/config/AppConfig.java
+++ src/main/java/technology/assessment/app/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,6 +17,7 @@ import java.util.concurrent.Executor;
 @EnableWebMvc
 @EnableAsync
 @EnableScheduling
+@EnableTransactionManagement
 public class AppConfig implements WebMvcConfigurer {
 
     @Bean(name = "asyncExecutor")
