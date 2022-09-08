b318172481672ddd31d484f322d199b37c114b9d
Odofin Oyejide <odofin@swipe.ng>
2022-09-08 09:51:51 +0100

    updated storeitem

--- src/main/java/technology/assessment/app/repository/listener/TransactionListener.java
+++ src/main/java/technology/assessment/app/repository/listener/TransactionListener.java
@@ -1,14 +1,11 @@
 package technology.assessment.app.repository.listener;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import technology.assessment.app.model.entity.StoreItem;
 import technology.assessment.app.model.entity.Transactions;
 import technology.assessment.app.model.entity.Users;
 import technology.assessment.app.model.enums.AccountType;
-import technology.assessment.app.repository.TransactionsRepo;
 import technology.assessment.app.util.AppUtil;
 import technology.assessment.app.util.CodeUtil;
 
